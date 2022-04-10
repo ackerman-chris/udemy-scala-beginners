aeda2c1fc9e53d1ea455dd51d3bd6ccc4efa02d2
Christopher Ackerman <ackerman.chris@outlook.com>
2022-04-10 09:20:37 -0500

    Changes

---
+++ src/main/scala/lectures/ValuesVariablesTypes.scala
@@ -0,0 +1,38 @@
+package part1basics
+
+/**
+ * Created by Christopher on 09-April-22
+ */
+object ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+  // VALS ARE IMMUTABLE
+
+  // COMPILER CAN INFER TYPES
+
+  val aString : String = "Hello"
+  val anotherString = "Goodbye"
+  println(aString + " " + anotherString)
+
+  // TYPES
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aSHort: Short = 3567
+  val aLong: Long = 46589874465897797L
+  val aFloat: Float = 2.0f
+  println(aFloat)
+  val aDouble: Double = 3.14
+  println(aDouble)
+
+  var aVariable: Int = 4
+
+  // VARS ARE MUTABLE
+
+  aVariable = 5 // side effects
+
+
+}
